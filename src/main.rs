766d73d27d0b068d3b13d83c2af0b9336127bd26
David Brown <davidb@davidb.org>
2022-03-05 22:41:23 -0700

    Deprecate the old UI code
    Now that the TUI code has full functionality, remove the old clunky UI.
    Allow the `--tui` argument, for now, for compatibility with any scripts.
    It can be removed for the next major release.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 //! Steno learning application.
 
-use crate::db::{Db, Work};
-use crate::input::{StrokeReader, Value};
+use crate::db::Db;
 use crate::lessons::Lesson;
 use crate::ui::Ui;
 use anyhow::Result;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::Write;
-use std::time::Duration;
 use structopt::StructOpt;
 
 mod db;
@@ -79,7 +75,8 @@ struct LearnCommand {
     new: Option<usize>,
 
     #[structopt(long = "tui")]
-    /// Enable the TUI interface
+    /// Enable the TUI interface (deprecated)
+    #[allow(dead_code)] // Deprecated: to be removed later
     tui: bool,
 }
 
@@ -90,23 +87,6 @@ struct Opt {
     command: Command,
 }
 
-/// RawMode captures raw mode in a RAII so that error exit will still clear raw mode.
-struct RawMode;
-
-impl RawMode {
-    fn new() -> Result<RawMode> {
-        enable_raw_mode()?;
-        Ok(RawMode)
-    }
-}
-
-impl Drop for RawMode {
-    fn drop(&mut self) {
-        // println!("\r\nDisabling raw mode\r");
-        disable_raw_mode().unwrap();
-    }
-}
-
 fn main() -> Result<()> {
     let opt = Opt::from_args();
     // println!("command: {:?}", opt);
@@ -114,18 +94,9 @@ fn main() -> Result<()> {
 
     match opt.command {
         Command::Learn(args) => {
-            let mut db = Db::open(&args.file)?;
-            if args.tui {
-                let mut ui = Ui::new(db, args.new)?;
-                ui.run()?;
-            } else {
-                let _raw = RawMode::new()?;
-
-                println!("Be sure Plover is configured to raw steno (no dict) and space after\r");
-                println!("Press <Esc> to exit\r\n");
-
-                learn(&mut db, args.new)?;
-            }
+            let db = Db::open(&args.file)?;
+            let mut ui = Ui::new(db, args.new)?;
+            ui.run()?;
         }
         Command::Import(args) => {
             let mut db = Db::open(&args.file)?;
@@ -161,109 +132,4 @@ fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-// Learn.
-fn learn(db: &mut Db, new: Option<usize>) -> Result<()> {
-    // let diag = Diagrammer::new();
-    let mut reader = StrokeReader::new();
-
-    loop {
-        let words = db.get_drills(5)?;
-
-        let head: Work;
-        let rest: &[Work];
-        if words.is_empty() {
-            if let Some(list) = new {
-                if let Some(work) = db.get_new(list)? {
-                    println!("\nLearn new word: {}: {}\r", work.text, work.strokes);
-                    head = work;
-                    rest = &[];
-                } else {
-                    println!("No more words left in list.\r");
-                    break;
-                }
-            } else {
-                println!("No more words left to learn.\r");
-                break;
-            }
-        } else {
-            let (h, r) = words.split_at(1);
-            head = h[0].clone();
-            rest = r;
-        };
-
-        println!("{} words due\r", db.get_due_count()?);
-        match learn_one(&mut reader, &head, rest)? {
-            None => break,
-            Some(count) => {
-                db.update(&head, count)?;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-// Quiz on a single word (with some context).  The value returned is the number of corrections
-// needed.  A "None" return means the user wishes to exit.
-fn learn_one(reader: &mut StrokeReader, work: &Work, rest: &[Work]) -> Result<Option<usize>> {
-    let mut stdout = std::io::stdout();
-    print!("Write: {}", work.text);
-
-    let mut corrected = 0;
-    let expect = work.strokes.linear();
-    let mut sofar = vec![];
-
-    if !rest.is_empty() {
-        print!(" |");
-    }
-    for r in rest {
-        print!(" {}", r.text);
-    }
-    println!("\r\n");
-    loop {
-        stdout.flush()?;
-
-        let stroke = match reader.read_stroke(Duration::MAX)? {
-            Value::Stroke(s) => s,
-            Value::Exit => {
-                println!("\r\n\nEarly exit\r");
-                return Ok(None);
-            }
-            Value::Resize(_, _) => continue,
-            Value::Timeout => continue,
-        };
-        print!("--> ");
-
-        if stroke.is_star() {
-            let _ = sofar.pop();
-            corrected += 1;
-        } else {
-            sofar.push(stroke);
-        }
-
-        let mut failed = false;
-
-        for (pos, &st) in sofar.iter().enumerate() {
-            if pos > 0 {
-                print!("/");
-            }
-            if pos >= expect.len() || expect[pos] != st {
-                failed = true;
-                print!("%");
-            }
-            print!("{}", st);
-        }
-        println!("\r");
-
-        if failed {
-            println!("  Should be: {}\r", work.strokes);
-        }
-
-        if sofar.len() == expect.len() && !failed {
-            println!("Correct! {}\r", corrected);
-            return Ok(Some(corrected));
-        }
-    }
 }
